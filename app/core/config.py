bc4e5b63b8177c71c9ed185618ea6a5321ae9f56
Muhammad Ayyan <muhammad.ayyan.aieng@gmail.com>
2026-07-12 02:50:29 +0300

    making config runnign for both local and deployment

--- app/core/config.py
+++ app/core/config.py
@@ -55,6 +55,10 @@ class Settings(BaseSettings):
             "http://localhost:5173",
             "http://127.0.0.1:3000",
             "http://127.0.0.1:5173",
+            "https://aetherlink-frontend.onrender.com",
+            "https://aetherlink.vercel.app",
+            # For development - allows any origin
+            "*"  # Add this for development
         ]
     )
     
